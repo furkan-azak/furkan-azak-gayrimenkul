470d4ed16aff989fef03bfd195d807e5362183f7
Furkan Azak <furak_3535@hotmail.com>
2026-01-08 18:39:41 +0300

    Fix ListingMap TS directives

--- components/ListingMap.tsx
+++ components/ListingMap.tsx
@@ -7,20 +7,17 @@ import L from "leaflet";
 type LatLng = { lat: number; lng: number };
 
 function fixLeafletIconsOnce() {
-  // @ts-expect-error internal
-  if ((L as any).__iconFixed) return;
-  // @ts-expect-error internal
-  (L as any).__iconFixed = true;
+  const anyL = L as any;
+  if (anyL.__iconFixed) return;
+  anyL.__iconFixed = true;
 
-  // @ts-expect-error private
+  // Leaflet default icon path fix
   delete (L.Icon.Default.prototype as any)._getIconUrl;
 
   L.Icon.Default.mergeOptions({
-    iconRetinaUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
     iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    shadowUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
   });
 }
 
